b0598a34c76db0aed5172ae1f2d8077c9d2f3e5a
Liliom Molnár <124668764+molnarlilko@users.noreply.github.com>
2026-04-17 18:22:28 +0200

    print, split lines in input.txt

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,10 +1,18 @@
 from pathlib import Path
 
-
 def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    lines = Path("input.txt").read_text(encoding="utf-8").splitlines()
 
+    for line in lines:
+        line = line.strip()
+
+        if "^" in line:
+            base, exp = line.split("^")
+            n = int(base) ** int(exp)
+        else:
+            n = int(line)
+
+        print(next_magic_num(n))
 
 if __name__ == "__main__":
     main()
