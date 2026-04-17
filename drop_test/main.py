949bd3850dc61c43e01629acc3484d917c1634ef
Liliom Molnár <124668764+molnarlilko@users.noreply.github.com>
2026-04-17 19:41:50 +0200

    read, split lines in input.txt

--- drop_test/main.py
+++ drop_test/main.py
@@ -2,9 +2,16 @@ from pathlib import Path
 
 
 def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    lines = Path("input.txt").read_text(encoding="utf-8").splitlines()
 
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue
+
+        NumberOfDrops_str, Height_str = line.split(",")
+        NumberOfDrops = int(NumberOfDrops_str.strip())
+        Height = int(Height_str.strip())
 
 if __name__ == "__main__":
     main()
