f68847e6659b11b6b98e3b9012a4571cffd12128
Liliom Molnár <124668764+molnarlilko@users.noreply.github.com>
2026-04-17 19:55:04 +0200

    min drops, print

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,6 +1,18 @@
 from pathlib import Path
 
 
+def min_num_of_drops(NumberOfDrop, Height):
+    dp = [0] * (NumberOfDrop + 1)
+    trials = 0
+
+    while dp[NumberOfDrop] < Height:
+        trials += 1
+        for i in range(NumberOfDrop, 0, -1):
+            dp[i] = dp[i] + dp[i-1] + 1
+
+    return trials
+
+
 def main():
     lines = Path("input.txt").read_text(encoding="utf-8").splitlines()
 
@@ -12,6 +24,9 @@ def main():
         NumberOfDrops_str, Height_str = line.split(",")
         NumberOfDrops = int(NumberOfDrops_str.strip())
         Height = int(Height_str.strip())
+
+        print(min_num_of_drops(NumberOfDrops, Height))
+
 
 if __name__ == "__main__":
     main()
